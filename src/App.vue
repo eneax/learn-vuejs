a3a597cf2b0873773c649dec675d80bb01fda388
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-09-12 11:46:04 +0200

    Form input state

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const title = ref('Tasks')
+const newTask = ref('')
+</script>
+
 <template>
   <main>
-    <h1>Hello World</h1>
+    <h1>{{ title }}</h1>
+    <form action="">
+      <label>
+        New Task
+        <input name="newTask" v-model="newTask">
+      </label>
+      <div class="button-container">
+        <button>Add</button>
+      </div>
+    </form>
   </main>
 </template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
